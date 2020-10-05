86dd14052270604a7b94a9b7fbe508ddc6a0bb5d
BTulkas <btulkas@gmail.com>
2020-10-05 18:35:42 +0300

    Added easter egg effects and nerfed to 1\1k. Added more strings

--- generate.js
+++ generate.js
@@ -1,13 +1,17 @@
 const baseName = [
-    "", "Pepe", "TunakTunak", "Pussy", "Jizz", "Blaze", "Meme", "Sk8er", "Smurf", "Doge", "RickRoll", "NotLacist", "BobNVajin", "ApacheHelicopter", "Simp", "Demon", "MadLad", "MadLass", "FforHarambe", "Virgin", "ExtraVirgin"
+    "", "Pepe", "TunakTunak", "Pussy", "Dick", "Jizz", "Blaze", "Meme", "Sk8er", "Smurf", "Doge", "RickRoll", "NotLacist",
+    "BobNVajin", "ApacheHelicopter", "Simp", "Demon", "MadLad", "MadLass", "FforHarambe", "Virgin", "ExtraVirgin", "Anon",
+    "Senpai", "Samurai", "Geisha", "Noob"
 ]
 
 const suffix = [
-    "", "420", "69", "Slayer", "Lord", "Masta", "Boy", "Girl", "nator", "Blazer", "Ganja", "Nerd", Math.floor((Math.random()*99)+1)+"", "XxX", "Face", "Queen", "King"
+    "", "420", "69", "Slayer", "Lord", "Masta", "Boy", "Girl", "nator", "Blazer", "Ganja", "Nerd",
+    Math.floor((Math.random()*99)+1)+"", "XxX", "Face", "Queen", "King", "Killer", "Flame"
 ]
 
 const prefix = [
-    "", "Dat", "The", "the", "Mr.", "Mrs.", "Lord", "Lady", "Ms.", "Ganja", "Super", "Captain", "TheIncredible", "Papa", "Mama", "A", "Big", "Lil'", "Master", "Supreme", "Holy"
+    "", "Dat", "The", "the", "Mr.", "Mrs.", "Lord", "Lady", "Ms.", "Ganja", "Super", "Captain", "TheIncredible", "Papa", "Mama",
+    "A", "Big", "Lil'", "Master", "Supreme", "Holy", "Sir", "Pro", "Omega", "Uber", "Ultra", "SSJ"
 ]
 
 const wrapper = [
@@ -20,10 +24,11 @@ function getRandom(max){
 }
 
 function generate(){
-    let roll = getRandom(10000);
+    let roll = getRandom(1000);
     let name;
-    if(roll===1){
+    if(roll===0){
         name = "TheLegend27";
+        $("#textWrapper").addClass("easterEgg")
     } else if(getRandom(4)===3) {
         let wrap = wrapper[getRandom(wrapper.length)];
         name = wrap[0]+prefix[getRandom(prefix.length)]+baseName[getRandom(baseName.length)]+suffix[getRandom(suffix.length)]+wrap[1];
